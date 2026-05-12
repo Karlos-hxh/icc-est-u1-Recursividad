32c5ede53438d8a0ff7098283a5bced299372967
Tu Nombre <danny27zh@gmail.com>
2026-05-11 19:53:47 -0500

    Recursividad

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Ejercicios ejercicios = new Ejercicios();
+        System.out.println("----Ejercicio 1----");
+        int rest1 = ejercicios. sumaConsecutivosPasos(5);
+        System.out.println("Resultado final --> "+rest1);
+
+    System.out.println("----------------------------------------");
+        System.out.println("----Ejercicio 2----");
+        int rest2 = ejercicios.getPotencia(2, 5);
+        System.out.println("Resultado final --> "+rest2);
+        System.out.println("----------------------------------------");
+        System.out.println("----Ejercicio 3----");
+        int rest3 = ejercicios.sumaDigitos(456);
+        System.out.println("Resultado final --> "+rest3);
     }
 }
